a9bcad5a0c2aba47e8b138d105453875065effd2
GSM-Godwin <godwinaliu29@gmail.com>
2024-08-22 18:10:50 +0100

    trained model

--- components/ShapeResults.tsx
+++ components/ShapeResults.tsx
@@ -14,6 +14,7 @@ const ShapeResults = ({ shapeType, necklineDesign }: { shapeType: string, neckli
 
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg mx-auto mt-8">
+      <h1 className='text-2xl font-bold mb-2 text-center text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-yellow-600 to-blue-700'>Blouse Recommender</h1>
       <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Your Shape Type: {shapeType}</h2>
       <div className="space-y-6">
         <div>
